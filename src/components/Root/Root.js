217faf122f31d27d3972a4ecf585f4679c97d3bc
sashaz <sasha.zahnitko@gmail.com>
2020-09-23 09:36:52 +1000

    Added "antd" and "styled-components". This is the new master. Will make a new dev and feature branch.

---
+++ src/components/Root/Root.js
@@ -0,0 +1,12 @@
+import { Table } from "antd";
+import React from "react";
+
+import { Wrapper } from "./styled";
+
+export const Root = () => {
+  return (
+    <Wrapper>
+      <Table   />
+    </Wrapper>
+  );
+};
---
+++ src/components/Root/styled.js
@@ -0,0 +1,4 @@
+import "antd/dist/antd.css";
+import styled from "styled-components";
+
+export const Wrapper = styled.div``;
