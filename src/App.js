7b5b0776ce44d0f926a18e2cefd255f63ca8ad66
sashaz <sasha.zahnitko@gmail.com>
2020-09-23 13:16:52 +1000

    So much random nonsense.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import { Root } from "./components/Root/Root";
 
 export const App = () => {
+  const [visible, setVisible] = useState(false);
+
   return (
     <>
-      <div>
-        <h1>This is my table</h1>
-        <div>Now I don't like.</div>
-        <div>Maybe now?</div>
-      </div>
-      <Root />
+      <Router>
+        <div>
+          <h1>This is my table</h1>
+          <div>Now I don't like.</div>
+          <div>Maybe now?</div>
+        </div>
+        <Switch>
+          <Route exact path="/">
+            <Root />
+          </Route>
+          <Route exact path="/fuck">
+            <div>
+              <div>Off</div>
+              <button onClick={() => setVisible((visible) => !visible)}>
+                Take me back
+              </button>
+              {visible && <div>There is no going back</div>}
+            </div>
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 };
